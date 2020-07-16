21cc0ff71efde177e14330f01a3aa6f3f5ddfa70
M@rlon <development.remarkable@gmail.com>
2020-07-16 14:29:30 +0200

    Observe viewModel data in MainActivity.kt

--- app/src/main/java/com/gmail/remarkable/development/airrybnik/MainActivity.kt
+++ app/src/main/java/com/gmail/remarkable/development/airrybnik/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.gmail.remarkable.development.airrybnik
 
 import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel by viewModels<MainViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewModel.response.observe(this, Observer { newResp ->
+            val tv = findViewById<TextView>(R.id.response_textView)
+            tv.text = newResp
+        })
+
     }
 }
