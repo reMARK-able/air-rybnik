bd049d4024bd55ace5b25702f2b50093c3cc7154
M@rlon <development.remarkable@gmail.com>
2020-07-16 14:27:00 +0200

    Add MainViewModel.kt

---
+++ app/src/main/java/com/gmail/remarkable/development/airrybnik/MainViewModel.kt
@@ -0,0 +1,31 @@
+package com.gmail.remarkable.development.airrybnik
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.gmail.remarkable.development.airrybnik.network.GiosApi
+import kotlinx.coroutines.launch
+
+class MainViewModel : ViewModel() {
+
+    private val _response = MutableLiveData<String>()
+    val response: LiveData<String>
+        get() = _response
+
+    init {
+        getGiosPm10fromRybnik()
+    }
+
+    private fun getGiosPm10fromRybnik() {
+        viewModelScope.launch {
+            try {
+                _response.value = GiosApi.retrofiteService.getPm10fromRybnik()
+            } catch (e: Exception) {
+                _response.value = "Error: ${e.message}" +
+                        "\n Cause: ${e.cause}"
+            }
+        }
+
+    }
+}
