1336663bebfb9385c970393490b953b6e2847f81
M@rlon <development.remarkable@gmail.com>
2020-07-17 13:41:33 +0200

    Change scalars converter to Mioshi with GiosSensorData

--- app/src/main/java/com/gmail/remarkable/development/airrybnik/MainViewModel.kt
+++ app/src/main/java/com/gmail/remarkable/development/airrybnik/MainViewModel.kt
@@ -5,13 +5,18 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.gmail.remarkable.development.airrybnik.network.GiosApi
+import com.gmail.remarkable.development.airrybnik.network.GiosSensorData
 import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
 
-    private val _response = MutableLiveData<String>()
-    val response: LiveData<String>
+    private val _response = MutableLiveData<GiosSensorData>()
+    val response: LiveData<GiosSensorData>
         get() = _response
+
+    private val _errorMessage = MutableLiveData<String>()
+    val errorMessage: LiveData<String>
+        get() = _errorMessage
 
     init {
         getGiosPm10fromRybnik()
@@ -22,7 +27,7 @@ class MainViewModel : ViewModel() {
             try {
                 _response.value = GiosApi.retrofitService.getPm10fromRybnik()
             } catch (e: Exception) {
-                _response.value = "Error: ${e.message}" +
+                _errorMessage.value = "Error: ${e.message}" +
                         "\n Cause: ${e.cause}"
             }
         }
