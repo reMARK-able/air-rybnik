607e526d0eea84adfe79c27a1f733d0dd957973d
M@rlon <development.remarkable@gmail.com>
2020-07-23 12:15:32 +0200

    Add AirRybnikApplication with @HiltAndroidApp

---
+++ app/src/main/java/com/gmail/remarkable/development/airrybnik/AirRybnikApplication.kt
@@ -0,0 +1,7 @@
+package com.gmail.remarkable.development.airrybnik
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class AirRybnikApplication : Application()
