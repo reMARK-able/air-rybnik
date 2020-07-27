a07c383e91dff6629466ad222b709338bb7a5ebf
M@rlon <development.remarkable@gmail.com>
2020-07-27 21:14:14 +0200

    Add LiveData for loading state views visibility

--- app/src/main/java/com/gmail/remarkable/development/airrybnik/pm10/BindingAdapters.kt
+++ app/src/main/java/com/gmail/remarkable/development/airrybnik/pm10/BindingAdapters.kt
@@ -1,5 +1,8 @@
 package com.gmail.remarkable.development.airrybnik.pm10
 
+import android.view.View
+import android.widget.ImageButton
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.gmail.remarkable.development.airrybnik.database.DatabaseSensorValue
@@ -16,5 +19,25 @@ fun TextView.getValueString(response: DatabaseSensorValue?) {
 fun TextView.getDateString(response: DatabaseSensorValue?) {
     response?.let {
         text = it.date.substringBeforeLast(":")
+    }
+}
+
+@BindingAdapter("hideRefreshButton")
+fun ImageButton.hideRefreshButton(isLoading: Boolean) {
+    isLoading.let {
+        visibility = when {
+            isLoading -> View.INVISIBLE
+            else -> View.VISIBLE
+        }
+    }
+}
+
+@BindingAdapter("showSpinner")
+fun ProgressBar.showSpinner(isLoading: Boolean) {
+    isLoading.let {
+        visibility = when {
+            isLoading -> View.VISIBLE
+            else -> View.GONE
+        }
     }
 }
