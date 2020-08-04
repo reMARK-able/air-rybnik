12b09f2fa9b1c8d7f1912667b7583107847b430d
M@rlon <development.remarkable@gmail.com>
2020-08-04 09:46:23 +0200

    Create notification channel

--- app/src/main/java/com/gmail/remarkable/development/airrybnik/pm10/RybnikPm10Fragment.kt
+++ app/src/main/java/com/gmail/remarkable/development/airrybnik/pm10/RybnikPm10Fragment.kt
@@ -1,11 +1,16 @@
 package com.gmail.remarkable.development.airrybnik.pm10
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.gmail.remarkable.development.airrybnik.R
 import com.gmail.remarkable.development.airrybnik.databinding.RybnikPm10FragmentBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -30,6 +35,28 @@ class RybnikPm10Fragment : Fragment() {
 
         viewDataBinding.lifecycleOwner = viewLifecycleOwner
 
+        createNotificationChannel(
+            getString(R.string.dataUpdate_notification_channel_id),
+            getString(R.string.dataUpdate_notification_channel_name)
+        )
+
         return viewDataBinding.root
+    }
+
+    private fun createNotificationChannel(channelId: String, channelName: String) {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel =
+                NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_HIGH)
+            notificationChannel.apply {
+                enableLights(true)
+                lightColor = Color.RED
+                enableVibration(true)
+                description = "Aktualizacja danych"
+            }
+            val notificationManager =
+                requireActivity().getSystemService(NotificationManager::class.java) as NotificationManager
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
     }
 }
