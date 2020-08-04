08fb3cb27b557383dc1c106db3c4cf4e42eb851c
M@rlon <development.remarkable@gmail.com>
2020-08-04 09:49:17 +0200

    Set testing notification on every data fetch

--- app/src/main/java/com/gmail/remarkable/development/airrybnik/util/NotificationUtils.kt
+++ app/src/main/java/com/gmail/remarkable/development/airrybnik/util/NotificationUtils.kt
@@ -3,6 +3,7 @@ package com.gmail.remarkable.development.airrybnik.util
 import android.app.NotificationManager
 import android.content.Context
 import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
 import com.gmail.remarkable.development.airrybnik.R
 
 private const val NOTIFICATION_ID = 0
@@ -21,4 +22,15 @@ fun NotificationManager.sendNotification(message: String, applicationContext: Co
         .setContentText(message)
 
     notify(NOTIFICATION_ID, builder.build())
+}
+
+/**
+ * Sends testing notification
+ */
+fun sendNewDataNotification(appContext: Context) {
+    val notificationManager = ContextCompat.getSystemService(
+        appContext,
+        NotificationManager::class.java
+    ) as NotificationManager
+    notificationManager.sendNotification("test powiadomienia", appContext)
 }
